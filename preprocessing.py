b4197bd70dd971c7110f0809a2e5191abd2797a1
Meiski Vedovatto <meiskiv@gmail.com>
2017-06-11 02:13:14 -0300

    Criar métodos para TF e finalizar

--- preprocessing.py
+++ preprocessing.py
@@ -5,9 +5,9 @@ from nltk import pos_tag
 from nltk.stem.snowball import SnowballStemmer
 import string
 import os
-import numpy
 from collections import Counter
 import math
+import numpy
 #import pandas as pd
 
 # TODO adicionar nas stopwords palavras como background, conclusions
@@ -59,8 +59,8 @@ def reduce_tostem(doc):
 
 
 def log_tf(doc_frequency):
-    print doc_frequency
-    print 'len(doc_frequency)', len(doc_frequency)
+    #print doc_frequency
+    #print 'len(doc_frequency)', len(doc_frequency)
     for n in range(len(doc_frequency)):
         doc_frequency[n] = list(doc_frequency[n])
         #print 'doc_frequency[n]: ', doc_frequency[n]
@@ -70,49 +70,44 @@ def log_tf(doc_frequency):
     return doc_frequency
 
 
-final_docs = []
-for i in os.listdir(os.getcwd()):
-    path_file = os.path.join(os.getcwd(), i)
+def count_frequencies(doc):
+    terms_plus_freq = Counter(doc).most_common()
+    return terms_plus_freq
+
+
+docs_terms = []
+for i in range(20):
+    path_file = os.path.join(os.getcwd(), (str(i+1)))
     document = read_file(path_file)
     document_sentences = seg_into_senteces(document)
     document_words = seg_into_words(document_sentences)
     final_words = remove_stopwords(document_words)
     punctuation_free = remove_punctuation(final_words)
-    stems = reduce_tostem(punctuation_free)
-    final_docs.append(stems)
+    docs_terms.append(reduce_tostem(punctuation_free))
+    terms_plus_frequencies = count_frequencies(docs_terms[i])
+    print 'terms_plus_frequencies',terms_plus_frequencies
+
+#TODO metodo pra fazer lista de todos os termos
+#def list_of_all_terms():
+#terms =[]
+
+
 
 terms =[]
-for i in range(len(final_docs)):
-    #print 'I: ', i
-    #print 'final_docs[',i,'] ',final_docs[i], 'len(final_docs): ',len(final_docs)
-    doc_terms = Counter(final_docs[i]).most_common() #final_docs[i] eh um vetor de termos do doc = doc_terms
-    print 'doc_terms',doc_terms
-    tf_ponderada = doc_terms
-    tf_ponderada[i] = log_tf(doc_terms)
-    'print tf_ponderada[i]: ',tf_ponderada[i]
-    'print tf_ponderada: ',tf_ponderada
-
-    #printa e etribui todos os termos
-    for j in range(len(doc_terms)):
-        #print '         len(doc_terms): ',len(doc_terms)
-        #print 'doc_terms[',i,']',doc_terms[j]
-        # doc_terms[j][0] acessa so o termo
-        terms.append(doc_terms[j][0])
-        print 'terms [',j,']: ',terms[j]
-
-#TODO utilizar panda para printar
-
-print 'final_terms length :',len(terms)
-print 'final terms TOTAL: ',terms
-final_terms = set(terms)
-final_terms = list(final_terms)
-print 'final_terms length sem duplicatas: ',len(final_terms)
-print 'final terms TOTAL sem duplicatas: ',final_terms
-print 'final terms [2]', final_terms[2]
+for i in range(len(docs_terms)):
+    terms_plus_frequencies = Counter(docs_terms[i]).most_common()
+    #tf_ponderada = doc_terms
+    #tf_ponderada[i] = log_tf(doc_terms)
+    #print 'print tf_ponderada[i]: ',tf_ponderada[i]
+    #print 'print tf_ponderada: ',tf_ponderada
+    for j in range(len(terms_plus_frequencies)):
+        terms.append(terms_plus_frequencies[j][0])
+final_terms = list(set(terms))
 
 
 
 
+#TODO utilizar panda para printar matriz
 ##############################################
 #REMEMBER: Index begins in 0
 #matriz = numpy.arange(1202*21).reshape(1202,21)
